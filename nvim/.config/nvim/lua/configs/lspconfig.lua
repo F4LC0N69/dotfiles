5153b89c303baa6999b64aaa314b6a8f6fccf139
saxam <saxam@saxams-MacBook-Air.local>
2025-09-29 16:36:31 +0545

    fixed lspconfig

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,23 +1,26 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
+local servers = { "html", "cssls", "ts_ls", "clangd", "zls", "pyright", "asm_lsp" }
+vim.lsp.enable(servers)
+--local lspconfig = require("lspconfig")
 --Added by me. If something isn't working, it's
 --probably because of this line below 8=====D
 --local util = require "lspconfig/util"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "clangd", "zls", "pyright", "asm_lsp" }
-local nvlsp = require("nvchad.configs.lspconfig")
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = nvlsp.on_attach,
-		on_init = nvlsp.on_init,
-		capabilities = nvlsp.capabilities,
-	})
-end
+-- note 2 this is where I probably fucked up.
+-- local servers = { "html", "cssls", "ts_ls", "clangd", "zls", "pyright", "asm_lsp" }
+-- local nvlsp = require("nvchad.configs.lspconfig")
+--
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+-- 	lspconfig[lsp].setup({
+-- 		on_attach = nvlsp.on_attach,
+-- 		on_init = nvlsp.on_init,
+-- 		capabilities = nvlsp.capabilities,
+-- 	})
+-- end
 
 -- configuring single server, example: rust
 -- lspconfig.rust_analyzer.setup {
